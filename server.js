f2898dee659e07abb91bdf59ff1025cfb6175216
George Karamanis <gkaramanis1@outlook.com>
2023-01-12 21:49:31 -0500

    finished writing file

--- server.js
+++ server.js
@@ -21,4 +21,24 @@ const sess = {
         secure: false,
         sameSite: 'strict',
     },
-}
+    resave: false,
+  saveUninitialized: true,
+  store: new SequelizeStore({
+    db: sequelize
+  })
+};
+
+app.use(session(sess));
+
+app.engine('handlebars', hbs.engine);
+app.set('view engine', 'handlebars');
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use(routes);
+
+sequelize.sync({ force: false }).then(() => {
+  app.listen(PORT, () => console.log('Now listening'));
+});
