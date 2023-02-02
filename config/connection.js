8d21fb65fd5fa557a29ec237b796ceb7a1f4862f
CDoolhoff <109476450+christopherdoolhoff@users.noreply.github.com>
2023-02-01 21:37:00 -0500

    fix Heroku

--- config/connection.js
+++ config/connection.js
@@ -2,7 +2,9 @@ const Sequelize = require('sequelize');
 require('dotenv').config();
 
 
-
+if (process.env.JAWSDB_URL){
+  sequelize = new sequelize(process.env.JAWSDB_URL);
+} else{
 const sequelize = new Sequelize(
   process.env.DB_NAME,
   process.env.DB_USER,
@@ -11,9 +13,9 @@ const sequelize = new Sequelize(
     host: '127.0.0.1',
     dialect: 'mysql',
     port: 3306,
-  }
+  },
 );
-
+}
 
 
 module.exports = sequelize;
